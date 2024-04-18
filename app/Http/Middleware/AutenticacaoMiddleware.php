7937b2cdba14b6876d3c778827f5e79c55f8a950
Igor Franca <igorfrancadiniz@gmail.com>
2024-04-18 17:27:39 -0300

    Autenticação de usuários

--- app/Http/Middleware/AutenticacaoMiddleware.php
+++ app/Http/Middleware/AutenticacaoMiddleware.php
@@ -16,11 +16,12 @@ class AutenticacaoMiddleware
 	 */
 	public function handle($request, Closure $next, $metodo_autenticacao)
 	{
-		// Verifica se o usuário possui acesso a rota
-		if(true) {
+		session_start();
+
+		if(isset($_SESSION['email']) && $_SESSION['email'] != '') {
 			return $next($request);
-		} else {
-			return Response('Acesso negado! Rota exige autenticação!!!');
+		}else {
+			return redirect()->route('site.login', ['erro' => 2]);
 		}
 	}
 }
